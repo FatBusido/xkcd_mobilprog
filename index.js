fa8032ecef7f6cd33b4a6fcfca6f073a0a3f2c66
unknown <kiss.viktor250@gmail.com>
2020-12-31 13:07:56 +0100

    Progress with favorite system and number request system

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-/**
+/**<>
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, SafeAreaView, StatusBar} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import randomComic from './xkcdAPI/.xkcd_Random.js';
+
+const App=()=>(
+    <SafeAreaView>
+        <StatusBar barStyle="auto"/>;
+        <randomComic></randomComic>;
+        
+
+    </SafeAreaView>
+)
+
+
+
+
 
 AppRegistry.registerComponent(appName, () => App);
--- xkcdAPI/xkcd_Random.js
+++ xkcdAPI/xkcd_Random.js
@@ -1,3 +1,52 @@
-import React, {useState} from 'react';
-import {View,Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View,Text, Image, Button} from 'react-native';
+import axios from 'axios';
 
+function app(){
+    var favorits=[];
+    const [comic,setComic] = useState(null);
+    useEffect(() =>{//Nem renderel minden kérésnél
+        const promise=axios.get('https://cors-anywhere.herokuapp.com/http://xkcd.com/info.0.json');
+        promise.then(Response =>setComic(Response.data))
+        .catch(error => console.log('error',error))
+        console.log('end of useEffect');
+
+    },[]);
+    const NumberComic=(number)=> {
+        
+        axios.get('https://cors-anywhere.herokuapp.com/http://xkcd.com/$(number)/info.0.json')
+        .then(Response=>setComic(Response.data))
+        .catch(error=>console.log('error',error));
+    }
+}
+if(!comic){
+    return<div>"loading..."</div>
+}
+
+
+//Favorithoz adás----
+const [comicNum, comicTittle]  = useState([
+    { id : "0", tittle : "Teszt"},
+  ]);
+const addElement = () => {
+    var newArray = [...comicNum , {id : idx, text: "tittle " + (idx+1) }];
+    incr(idx + 1);
+    console.log(comicNum.length);
+    setExampleState(newArray);
+    comicTittle(newArray);
+  }
+//Favorithoz adás vége---
+return{ //<>
+    <Text> comic.tittle  </Text>;
+    <Image
+    style={styles.tinyLogo}
+    source={{ comic.Image
+    }}
+  />
+  <button tittle="SetFavorite" onPress={addElement}/> 
+
+
+
+};
+
+export default app;
