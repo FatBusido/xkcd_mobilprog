0d39528af28a00b88c695532c7b4e118594867ec
unknown <kiss.viktor250@gmail.com>
2021-01-18 13:09:30 +0100

    finished last things

--- xkcdAPI/xkcd_Random.js
+++ xkcdAPI/xkcd_Random.js
@@ -3,7 +3,6 @@ import {View,Text, Image, Button} from 'react-native';
 import axios from 'axios';
 
 function app(){
-    var favorits=[];
     const [comic,setComic] = useState(null);
     useEffect(() =>{//Nem renderel minden kérésnél
         const promise=axios.get('https://cors-anywhere.herokuapp.com/http://xkcd.com/info.0.json');
@@ -12,15 +11,22 @@ function app(){
         console.log('end of useEffect');
 
     },[]);
-    const NumberComic=(number)=> {
-        
+    const NumberComic=(number)=> {//Megadott számú comic lekérése
         axios.get('https://cors-anywhere.herokuapp.com/http://xkcd.com/$(number)/info.0.json')
         .then(Response=>setComic(Response.data))
         .catch(error=>console.log('error',error));
     }
+    const RandomComic=()=> {//Random comics
+    const min = 1;
+    const max = 2400;
+    const rand = min + Math.random() * (max - min);
+      axios.get('https://cors-anywhere.herokuapp.com/http://xkcd.com/$(rand)/info.0.json')
+      .then(Response=>setComic(Response.data))
+      .catch(error=>console.log('error',error));
+  }
 }
 if(!comic){
-    return<div>"loading..."</div>
+    return<div>"error..."</div>
 }
 
 
@@ -29,21 +35,23 @@ const [comicNum, comicTittle]  = useState([
     { id : "0", tittle : "Teszt"},
   ]);
 const addElement = () => {
-    var newArray = [...comicNum , {id : idx, text: "tittle " + (idx+1) }];
+    var newArray = [...comicNum , {id : comic.number, tittle: comic.tittle + (idx+1) }];
     incr(idx + 1);
-    console.log(comicNum.length);
     setExampleState(newArray);
     comicTittle(newArray);
   }
 //Favorithoz adás vége---
 return{ //<>
-    <Text> comic.tittle  </Text>;
+    <Text comic.tittle  />;
     <Image
     style={styles.tinyLogo}
     source={{ comic.Image
     }}
   />
   <button tittle="SetFavorite" onPress={addElement}/> 
+  <button tittle="RandomComic" onPress={RandomComic}/> 
+  <TextField id="standard-basic" label="Number" />
+  <button tittle="SetFavorite" onPress={NumberComic()}/>
 
 
 
